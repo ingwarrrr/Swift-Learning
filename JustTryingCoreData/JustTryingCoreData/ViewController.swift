70a05ad01f93cc59a5a8eec8e87efb734031b3c1
ingwarrrr <igorek0706@gmail.com>
2022-03-08 19:06:01 +0300

    initial commit

---
+++ JustTryingCoreData/JustTryingCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustTryingCoreData
+//
+//  Created by Igor on 08.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

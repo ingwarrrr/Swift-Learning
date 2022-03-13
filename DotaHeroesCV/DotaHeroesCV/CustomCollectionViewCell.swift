246045fea631e6177f82ba4efbfddb8cdf564b0a
ingwarrrr <igorek0706@gmail.com>
2022-03-14 00:45:44 +0300

    Dota 2 heroes api is used as a data source for collectionView

---
+++ DotaHeroesCV/DotaHeroesCV/CustomCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCollectionViewCell.swift
+//  DotaHeroesCV
+//
+//  Created by Igor on 13.03.2022.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+}

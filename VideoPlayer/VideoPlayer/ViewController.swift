bb7488123fe57bc42c16c0b9565adc6cd303e793
ingwarrrr <igorek0706@gmail.com>
2022-04-01 19:29:32 +0300

    initial commit

---
+++ VideoPlayer/VideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Igor on 01.04.2022.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
